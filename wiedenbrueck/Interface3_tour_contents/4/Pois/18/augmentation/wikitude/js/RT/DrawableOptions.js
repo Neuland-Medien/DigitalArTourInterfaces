c5700fc4d0cd58518df8aefb4c4556b47740a1b8
DESKTOP-SV1MC5G\Tom Quinders <tomquinders@gmail.com>
2018-08-14 14:20:51 +0200

    youth modified: removed 20 abcd, 18 and 11 (shouldnt be houses in the tour), swapped audio to the right files, exchanged 19 video so its playable on iOS

--- wiedenbrueck/Interface3_tour_contents/4/Pois/18/augmentation/wikitude/js/RT/DrawableOptions.js
+++
@@ -1,2 +0,0 @@
-var options_audioaroff = {"name":"audioaroff","scale":{"x":1.2010989192,"y":1.2010989192,"z":1.2010989192},"translate":{"x":0.48006599,"y":0.3092719,"z":0,"global":{"x":0,"y":0,"z":0}},"rotate":{"x":0,"y":0,"z":0,"global":{"x":0,"y":0,"z":0}}}
-var options_audioARon = {"name":"audioARon","scale":{"x":1.2010989192,"y":1.2010989192,"z":1.2010989192},"translate":{"x":0.48006599,"y":0.3092719,"z":0,"global":{"x":0,"y":0,"z":0}},"rotate":{"x":0,"y":0,"z":0,"global":{"x":0,"y":0,"z":0}}}
