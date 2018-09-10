70929d94a796f86d4ced17b9bce8832b9e0d3338
Karen Börner <karen.boerner@raumtaenzer.com>
2018-09-10 10:25:41 +0200

    position corrected

--- wiedenbrueck/Interface3_tour_contents/3/Pois/10/augmentation/wikitude/js/RT/DrawableOptions.js
+++ wiedenbrueck/Interface3_tour_contents/3/Pois/10/augmentation/wikitude/js/RT/DrawableOptions.js
@@ -1,1 +1,1 @@
-var options_Fuerst = {"name":"Fuerst","scale":{"x":1.3,"y":1.3},"translate":{"x":-0.059256470000000054,"y":-0.48,"z":0.27,"global":{"x":0,"y":0,"z":0}},"rotate":{"x":0,"y":0,"z":0,"global":{"x":0,"y":0,"z":0}}}
+var options_Fuerst = {"name":"Fuerst","scale":{"x":0.98,"y":0.98},"translate":{"x":0,"y":-0.27,"z":0,"global":{"x":0,"y":0,"z":0}},"rotate":{"x":0,"y":0,"z":0,"global":{"x":0,"y":0,"z":0}}}
