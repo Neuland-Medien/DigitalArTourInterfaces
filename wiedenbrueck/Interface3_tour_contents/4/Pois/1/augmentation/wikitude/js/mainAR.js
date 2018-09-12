bff11c2e169bcd271ad4c3bf56126d65a8776817
Karen Börner <karen.boerner@raumtaenzer.com>
2018-09-12 13:49:24 +0200

    including new marker in youth tour

--- wiedenbrueck/Interface3_tour_contents/4/Pois/1/augmentation/wikitude/js/mainAR.js
+++ wiedenbrueck/Interface3_tour_contents/4/Pois/1/augmentation/wikitude/js/mainAR.js
@@ -1,47 +1,47 @@
 var trackable_rhedaMarker02 = new RTTrackable("marker1");
+var trackable_rhedaMarker022 = new RTTrackable("marker1_2");
 var audioaroff = new RTImage("assets/audioaroff.png",1,options_audioaroff,trackable_rhedaMarker02);
 var audioARon = new RTImage("assets/audioARon.png",1,options_audioARon,trackable_rhedaMarker02);
+var audioaroff2 = new RTImage("assets/audioaroff.png",1,options_audioaroff,trackable_rhedaMarker022);
+var audioARon2 = new RTImage("assets/audioARon.png",1,options_audioARon,trackable_rhedaMarker022);
 var sound = new AR.Sound("assets/audio.mp3");
 sound.load();
 audioaroff.onClickAdditions=function(){
   sound.play();
   audioARon.addToCam();
   audioaroff.removeFromCam();
+  audioARon2.addToCam();
+  audioaroff2.removeFromCam();
 };
 
 audioARon.onClickAdditions=function(){
   sound.pause();
   audioaroff.addToCam();
   audioARon.removeFromCam();
+  audioaroff2.addToCam();
+  audioARon2.removeFromCam();
 };
-
 sound.onFinishedPlaying=function(){
   audioaroff.addToCam();
   audioARon.removeFromCam();
+  audioaroff2.addToCam();
+  audioARon2.removeFromCam();
 }
-audioaroff.addToCam();  
+audioaroff.addToCam();
+audioaroff2.addToCam();
 
 var World = {
     switchContentToInfo: function() {
       sound.pause();
       audioARon.removeFromCam();
       audioaroff.removeFromCam();
-
-      //document.getElementById("clara").style.visibility = "visible";
-      document.getElementById("louis").style.visibility = "visible";
     },
     switchContentToAR: function() {
       audioaroff.addToCam();
-      //document.getElementById("clara").style.visibility = "hidden";
-      document.getElementById("louis").style.visibility = "hidden";
     },
     turnEverythingOff: function() {
       sound.pause();
       audioARon.removeFromCam();
       audioaroff.removeFromCam();
-
-      //document.getElementById("clara").style.visibility = "hidden";
-      document.getElementById("louis").style.visibility = "hidden";
-        
     }
 };
