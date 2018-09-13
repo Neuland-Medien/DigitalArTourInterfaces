d347d7fafeb135f41064778a2bb825200aacea8e
Karen Börner <karen.boerner@raumtaenzer.com>
2018-09-13 12:34:48 +0200

    shows just one person at youth tour number three

--- wiedenbrueck/Interface3_tour_contents/4/Pois/3/augmentation/wikitude/js/claralouis.js
+++ wiedenbrueck/Interface3_tour_contents/4/Pois/3/augmentation/wikitude/js/claralouis.js
@@ -1,14 +1,14 @@
 var World = {
     switchContentToInfo: function() {
-      document.getElementById("clara").style.visibility = "visible";
+      //document.getElementById("clara").style.visibility = "visible";
       document.getElementById("louis").style.visibility = "visible";
     },
     switchContentToAR: function() {
-      document.getElementById("clara").style.visibility = "hidden";
+      //document.getElementById("clara").style.visibility = "hidden";
       document.getElementById("louis").style.visibility = "hidden";
     },
     turnEverythingOff: function() {
-      document.getElementById("clara").style.visibility = "hidden";
+      //document.getElementById("clara").style.visibility = "hidden";
       document.getElementById("louis").style.visibility = "hidden";
     }
 };
